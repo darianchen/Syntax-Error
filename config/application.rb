132cfa4c79641b3aa516dfd113b2cac77026c627
Darian Chen <darianychen@gmail.com>
2022-10-13 14:04:11 -0400

    Update backend for heroku

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.railties_order = [:all, :main_app]
+
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
       key: '_auth_me_session',
