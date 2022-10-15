0c4cfe83b0a399e6ed9457118e4f08b8810b0803
Darian Chen <darianychen@gmail.com>
2022-10-14 22:28:22 -0400

    Add RESTful actions to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,26 @@
 class Api::UsersController < ApplicationController
   wrap_parameters include: User.attribute_names + ['password']
 
+  def index
+    @users = User.all
+    render :index
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       login! @user
       render 'api/users/show'
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    if @user
+      render :show
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
