e4708727767b0b9237308d570c56644d31640097
Darian Chen <darianychen@gmail.com>
2022-10-20 10:32:49 -0400

    Add tags page, implement later

---
+++ frontend/src/components/TagIndexComponent/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import LeftSidebar from "../LeftSidebarComponent";
+import './index.css'
+
+
+const TagIndexComponent = () => {
+
+
+    return (
+        <>
+            <div className="page-content">
+                <div></div>
+                <LeftSidebar/>
+
+            </div>
+        </>
+    )
+     
+}
+
+export default TagIndexComponent
