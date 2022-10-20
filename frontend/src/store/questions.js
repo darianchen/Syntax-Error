c6b102b98f8e120170cb0d2dd6c31f8e57cf061f
Darian Chen <darianychen@gmail.com>
2022-10-20 09:38:12 -0400

    Add questions slice of state and reducer

---
+++ frontend/src/store/questions.js
@@ -0,0 +1,88 @@
+import csrfFetch from "./csrf";
+
+export const RECEIVE_QUESTIONS = "questions/RECEIVE_QUESTIONS";
+export const RECEIVE_QUESTION = "questions/RECEIVE_QUESTION";
+export const REMOVE_QUESTION = "questions/REMOVE_QUESTION";
+
+export const receiveQuestions = questions => ({
+    type: RECEIVE_QUESTIONS,
+    questions
+});
+
+export const receiveQuestion = question => ({
+    type: RECEIVE_QUESTION,
+    question
+});
+
+export const removeQuestion = questionId => ({
+    type: REMOVE_QUESTION,
+    questionId
+});
+
+export const getQuestion = questionId => ({questions}) => questions ? questions[questionId] : null;
+export const getQuestions = ({questions}) => questions ? Object.values(questions) : [];
+
+export const fetchQuestion = questionId => async dispatch => {
+    const res = await csrfFetch(`/api/questions/${questionId}`);
+    const data = await res.json();
+    dispatch(receiveQuestion(data.question));
+};
+
+export const fetchQuestions = () => async dispatch => {
+    const res = await csrfFetch(`/api/questions`);
+    const data = await res.json();
+    dispatch(receiveQuestions(data));
+};
+
+export const deleteQuestion = questionId => async dispatch => {
+    debugger
+    await csrfFetch(`/api/questions/${questionId}`, {method: "DELETE"});
+    dispatch(removeQuestion(questionId));
+};
+
+export const updateQuestion = question=> async dispatch => {
+    debugger
+    const res = await csrfFetch(`/api/questions/${question.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(question),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    dispatch(receiveQuestion(data));
+  };
+  
+
+
+
+// export const createQuestion = question => async dispatch => {
+//     const res = await csrfFetch('/api/questions', {
+//         method: 'POST',
+//         body: JSON.stringify(question),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+//     const data = await res.json()
+//     dispatch(getQuestion(data))
+// };
+
+const questionsReducer = (state= {}, action) => {
+    Object.freeze(state);
+    const nextState = {...state};
+    switch (action.type) {
+        case RECEIVE_QUESTIONS:
+            return {...state, ...action.questions};
+        case RECEIVE_QUESTION:
+            nextState[action.question.id] = action.question;
+            return nextState;
+        case REMOVE_QUESTION:
+            delete nextState[action.userId];
+            return nextState;
+        default:
+            return state;
+    }
+};
+
+export default questionsReducer;
