67bdeff22740c44224334fdae553a39c583d2992
Darian Chen <darianychen@gmail.com>
2022-10-20 09:35:59 -0400

    Add frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from 'react-router-dom'
-import LoginFormPage from "./components/LoginFormPage";
+import LoginFormPage from "./components/LoginFormComponent";
 import Navbar from "./components/Nav";
 import Navigation from "./components/Navigation";
-import SignupFormPage from "./components/SignupFormPage";
-import UserPage from "./components/UserShow";
-import SplashPage from "./components/SplashPage";
-import UsersPage from "./components/UsersIndexPage";
+import SignupFormPage from "./components/SignupFormComponent";
+import UserPage from "./components/UserShowComponent";
+import SplashPage from "./components/Splash";
+import UsersPage from "./components/UserIndexComponent";
+import QuestionShow from "./components/QuestionShowComponent";
+import QuestionIndex from "./components/QuestionIndexComponent";
+import QuestionForm from "./components/QuestionFormComponent";
+import TagIndexComponent from "./components/TagIndexComponent";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "./store/users";
+import QuestionEditForm from "./components/QuestionEditFormComponent";
+import { fetchQuestions } from "./store/questions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect( () => {
+    dispatch(fetchUsers());
+    dispatch(fetchQuestions());
+  },[])
+
   return (
     <>
       <Navbar />
-      <Navigation />
         <Switch>
+
         <Route exact path="/">
               <SplashPage />
         </Route>
+
           <Route path="/login">
               <LoginFormPage />
           </Route>
+
           <Route path="/signup">
               <SignupFormPage />
           </Route>
+
           <Route exact path="/users/:userId">
               <UserPage/>
           </Route>
+
           <Route exact path="/users">
               <UsersPage/>
           </Route>
+
+          <Route exact path="/questions">
+              <QuestionIndex/>
+          </Route>
+
+          <Route exact path="/questions/ask">
+              <QuestionForm/>
+          </Route>
+
+          <Route exact path="/questions/:questionId">
+              <QuestionShow/>
+          </Route>
+
+          <Route exact path="/questions/:questionId/edit">
+              <QuestionEditForm/>
+          </Route>
+
+          <Route exact path="/tags">
+              <TagIndexComponent/>
+          </Route>
+            
       </Switch>
     </>
   );
