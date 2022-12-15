1c8ff8f94facf2a60d3bd8e83fed9c1af8a8fbca
Darian Chen <darianychen@gmail.com>
2022-12-15 03:17:53 -0800

    Create answer migration

---
+++ db/migrate/20221215031753_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.integer :answerer_id, null: false
+      t.integer :question_id, null: false
+      t.text :description, null: false
+      t.references :editor, foreign_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
