c42326f27f33f166443928b3daa31a97562232db
Darian Chen <darianychen@gmail.com>
2023-01-02 09:31:36 -0800

    Add tags and votes to database

---
+++ db/migrate/20230102091500_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration[7.0]
+    def change create_table :votes do |t|
+        t.integer :voter_id, null: false
+        t.boolean :votable, null: false
+        t.string :votable_type, null: false
+        t.timestamps
+        end
+    end
+end
+    
---
+++ db/migrate/20230102091857_create_tags.rb
@@ -0,0 +1,7 @@
+class CreateTags < ActiveRecord::Migration[7.0]
+    def change create_table :tags do |t|
+        t.string :name, null: false
+        t.timestamps
+        end
+    end
+end
