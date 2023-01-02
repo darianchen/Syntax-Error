3424a1880e4e89553189381f84e09b7983f1969d
Darian Chen <darianychen@gmail.com>
2023-01-02 09:31:36 -0800

    Fix issue

--- db/migrate/20230102091857_create_tags.rb
+++ db/migrate/20230102091857_create_tags.rb
@@ -1,7 +1,8 @@
 class CreateTags < ActiveRecord::Migration[7.0]
-    def change create_table :tags do |t|
-        t.string :name, null: false
-        t.timestamps
-        end
+  def change
+    create_table :tags do |t|
+      t.string :name, null: false
+      t.timestamps
     end
+  end
 end
