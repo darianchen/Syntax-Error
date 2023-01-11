cd1ba557239d31d83203a84b9f1078498b6bd021
Darian <darianychen@gmail.com>
2023-01-11 07:53:17 -0800

    Refactor vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,14 +1,4 @@
 class Vote < ApplicationRecord
     validates :voter_id, :post_id, presence: true
-    validates :voter_id, uniqueness: {scope: :post_id}
     validates :vote, inclusion: { in: [true, false]}
-
-    belongs_to :question,
-        foreign_key: :post_id,
-        class_name: :Question
-
-    belongs_to :voter,
-        foreign_key: :voter_id,
-        class_name: :User,
-        optional: true
 end
