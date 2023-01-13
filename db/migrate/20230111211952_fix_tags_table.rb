c18946365db88cc9b4452d5ae403cb9a3e22c4b2
Darian <darianychen@gmail.com>
2023-01-12 21:45:19 -0800

    Try to push again

--- db/migrate/20230111211952_fix_tags_table.rb
+++ db/migrate/20230111211952_fix_tags_table.rb
@@ -1,11 +1,11 @@
 class FixTagsTable < ActiveRecord::Migration[7.0]
     def change
-      if column_exists?(:tags,: :name)
+      if column_exists?(:tags, :name)
         Tag.update_all(name: '-')
         change_column_null :tags, :name, false
       end
 
-      if column_exists?(:tags,: :question_id)
+      if column_exists?(:tags, :question_id)
         remove_column :tags, :question_id, :integer
       end
     
