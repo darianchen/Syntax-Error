96a0d1f7f054af31c6a1f243ee743835fdd9048a
Darian <darianychen@gmail.com>
2023-01-12 20:13:31 -0800

    Fix error

--- db/migrate/20230111202721_refactor_votes_and_tags.rb
+++ db/migrate/20230111202721_refactor_votes_and_tags.rb
@@ -1,6 +1,6 @@
 class RefactorVotesAndTags < ActiveRecord::Migration[7.0]
   def change
     add_column :votes, :post_type, :boolean, :default => false
-    remove_column :tags, :taglist
   end
 end
+    # remove_column :tags, :taglist
