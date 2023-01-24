113f2ba7b1f08f7bc56500e21ba68d6e00c71450
Darian Chen <darianychen@gmail.com>
2023-01-23 23:28:16 -0500

    Add has_many questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@
 #
 class User < ApplicationRecord
     has_secure_password
-  
+    has_many :questions, dependent: :destroy
+
     validates :display_name,
       presence: true,
       length: { maximum: 30 }, 
