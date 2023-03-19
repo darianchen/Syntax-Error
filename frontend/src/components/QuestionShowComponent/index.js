e46b58f188a651694447518f80dae0de40640db3
Darian <darianychen@gmail.com>
2023-03-19 13:51:52 -0400

    Fix styling on tag index and question show, fix scrolling problem

--- frontend/src/components/QuestionShowComponent/index.js
+++ frontend/src/components/QuestionShowComponent/index.js
@@ -41,64 +41,62 @@ const QuestionShow = () => {
     dispatch(fetchAllQuestions())
   }
 
-  if (question) {
-    return (
-      <>
-          <div className="question-show-content">
-            <div className="title-and-button">
-              <h1 className="question-title">{question.title}</h1>
-              <button
-                onClick={handleClick}
-                className="question-show-ask-question-button"
-              >
-                Ask Question
-              </button>
+  return (
+        <div className="question-show-content">
+          { question && 
+          <>  
+          <div className="title-and-button">    
+          <h1 className="question-title">{question?.title}</h1>
+            <button
+              onClick={handleClick}
+              className="question-show-ask-question-button"
+            >
+            Ask Question
+            </button>
+          </div>
+          <ul className="stats">
+            <li>
+              Asked <span>{moment(question.createdAt).fromNow()}</span> by{" "}
+              <span className="displayName">{question.author}</span>
+            </li>
+          </ul>
+          <hr/>
+          <div className="question-show-bottom" >
+            <Vote key={`question${question.id}`} post={question} sessionUser={sessionUser} isAnswer={false} dispatchPost={dispatchQuestion}/>
+            <div className="question-body">
+              {question.body}
+              <div className="question-tags">
+                <TagsComponent tags={question.tagsAttributes} />
+              </div>
             </div>
-            <ul className="stats">
-              <li>
-                Asked <span>{moment(question.createdAt).fromNow()}</span> by{" "}
-                <span className="displayName">{question.author}</span>
-              </li>
-              <li></li>
-            </ul>
-            <hr></hr>
-            <div className="question-show-bottom" >
-              <Vote key={`question${question.id}`} post={question} sessionUser={sessionUser} isAnswer={false} dispatchPost={dispatchQuestion}/>
-              <div className="question-body">
-                {question.body}
-                <div className="question-tags">
-                  <TagsComponent tags={question?.tagsAttributes} />
+          </div>
+          {sessionUser && sessionUser.id === question.authorId ? (
+            <div className="crud-functions">
+              <div style={{display:"flex"}}>
+                <div className="edit-delete" style={{ marginRight: "5px" }}>
+                  <Link to={`/questions/${questionId}/edit`}>Edit</Link>
+                </div>
+                <div className="edit-delete" onClick={handleDelete}>
+                  Delete
                 </div>
               </div>
             </div>
-            {sessionUser && sessionUser.id === question.authorId ? (
-              <div className="crud-functions">
-                <div style={{display:"flex"}}>
-                  <div className="edit-delete" style={{ marginRight: "5px" }}>
-                    <Link to={`/questions/${questionId}/edit`}>Edit</Link>
-                  </div>
-                  <div className="edit-delete" onClick={handleDelete}>
-                    Delete
-                  </div>
-                </div>
-              </div>
-            ) : (
-              ""
-            )}
-            <div className="answer-count">{question.answerCount} answer{question.answerCount === 1 ? "" : "s"}</div>
-            <AnswerIndex />
-            {sessionUser ? <AnswerForm questionId={questionId} /> : ""}
-            {sessionUser ? (
-              ""
-            ) : (
-              <div style={{ marginTop: "15px" }}>
-                Please <a href="/login">login</a> to answer
-              </div>
-            )}
-        </div>
-      </>
-    );
-  }
+          ) : (
+            ""
+          )}
+          <div className="answer-count">{question.answerCount} answer{question.answerCount === 1 ? "" : "s"}</div>
+          <AnswerIndex />
+          {sessionUser ? <AnswerForm questionId={questionId} /> : ""}
+          {sessionUser ? (
+            ""
+          ) : (
+            <div style={{ marginTop: "15px" }}>
+              Please <a href="/login">login</a> to answer
+            </div>
+          )} 
+          </>}   
+      </div>
+  );
 };
 
 export default QuestionShow;
--- frontend/src/components/TagIndexComponent/index.js
+++ frontend/src/components/TagIndexComponent/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import LeftSidebar from "../LeftSidebarComponent";
 import './index.css'
 import { getTags, fetchTags } from "../../store/tags";
 import TagsComponent from "./tags"
